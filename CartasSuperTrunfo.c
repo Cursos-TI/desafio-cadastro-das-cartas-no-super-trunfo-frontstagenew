fde8c6ea10d98acdf5e4872c242c21691d9768e7
frontstagenew <penelopepg256@gmail.com>
2025-02-17 17:44:48 +0000

    trabalhoconcluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,11 +8,28 @@
 
 int main() {
 
-    int populacao
-    float area;
-    float pib;
+    //criando variaveis
+    int populacao;
+    float area, pib;
     int pontosturisticos;
 
+    //inserindo dados de entrada para usuario digitar
+printf("Digite a quantidade população: \n");
+scanf("%d", &populacao);
 
+printf("Digite a area: \n");
+scanf("%f", &area);
+
+printf("Digite o PIB: \n");
+scanf("%f", &pib);
+
+printf("Digite a qtd de pontos turisticos: \n");
+scanf("%d", &pontosturisticos);
+
+//imprimindo na tela informações digitadas pelo usuario
+printf("População: %d \n", populacao);
+printf("Area: %f \n", area);
+printf("PIB: %f \n", pib);
+printf("Pontos Turisticos: %d \n", pontosturisticos);
     return 0;
 }
